e19a16983fe2d82e5f83479ec76401e1dd353083
arj101 <arjunanmr101@gmail.com>
2023-11-05 19:12:56 +0530

    summation row

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Linq;
 using MigraDoc.DocumentObjectModel;
 using MigraDoc.Rendering;
 namespace ConsoleApp3
@@ -7,7 +8,7 @@ namespace ConsoleApp3
     internal class Program
     {
 
-        static void AddTable(Section section, Color primaryColor, string[] colSizes, string[] colHeadings, string[][] rowDatas, int fontSize = 8, string fontName = "Noto Sans")
+        static void AddTable(Section section, Color primaryColor, string[] colSizes, string[] colHeadings, string[][] rowDatas, int fontSize = 8, string fontName = "Noto Sans", bool summationRow = false)
         {
 
             var table = section.AddTable();
@@ -23,11 +24,11 @@ namespace ConsoleApp3
             headingFont.Bold = true;
             headingFont.Name = fontName;
             headingFont.Size = fontSize;
+            var primaryColorHeading = Color.FromArgb(150, (byte)primaryColor.R, (byte)primaryColor.G, (byte)primaryColor.B);
             for (int i = 0; i < colHeadings.Length && i < colSizes.Length; i++)
             {
                 var headingPara = headingRow.Cells[i].AddParagraph();
                 headingPara.AddFormattedText(colHeadings[i], headingFont);
-                var primaryColorHeading = Color.FromArgb(150, (byte)primaryColor.R, (byte)primaryColor.G, (byte)primaryColor.B);
                 headingRow.Cells[i].Shading.Color = primaryColorHeading;
             }
             headingRow.Borders.Bottom.Width = 2;
@@ -40,20 +41,26 @@ namespace ConsoleApp3
             dataFont.Name = fontName;
             dataFont.Size = fontSize;
 
+
             for (int i = 0; i < rowDatas.Length; i++)
             {
                 string[] rowData = rowDatas[i];
                 var row = table.AddRow();
 
 
+                if ((i + 1) % 2 == 0) row.Shading.Color = primaryColorLight;
+                if (i == rowDatas.Length - 1 && summationRow)
+                {
+                    row.Borders.Top.Width = 2;
+                }
                 for (int j = 0; j < colSizes.Length && j < rowData.Length; j++)
                 {
                     var para = row.Cells[j].AddParagraph();
-                    para.AddFormattedText(rowData[j], dataFont);
-                    if ((i + 1) % 2 == 0) row.Cells[j].Shading.Color = primaryColorLight;
+                    para.AddFormattedText(rowData[j], i < rowDatas.Length - 1 || !summationRow ? dataFont : headingFont);
                 }
-
             }
+
+
 
             table.Borders.Color = Color.FromArgb(255, 0, 0, 0);
             table.Borders.Width = 1.0;
@@ -110,7 +117,7 @@ namespace ConsoleApp3
                 "1.5cm",
                 "1.8cm",
             };
-            string[] colHeads = { 
+            string[] colHeads = {
                 "Client Address",
                 "Type of Customer",
                 "Test Lab",
@@ -201,10 +208,32 @@ namespace ConsoleApp3
         new string[]
         {
             "666 Ram Manohar Lohia Road, Madurai, India","Regular","LMN Labs","Microbiological Analysis","Bacterial Test 6","WO15151","2023-11-22","2023-12-02","3","90","270","TRN131","2023-12-07","2023-11-27","2023-12-03","Accredited"
-        }
+        },
             };
 
-            AddTable(section, primaryColor, colSizes, colHeads, csvData, fontSize: 8);
+            var totalNoOfMaterial = 0;
+            var totalTestCharge = 0;
+
+            for (int i = 0; i < csvData.Length; i++)
+            {
+                try
+                {
+                    var noOfMaterial = Int32.Parse(csvData[i][8]);
+                    totalNoOfMaterial += noOfMaterial;
+                }
+                catch { }
+                try
+                {
+                    var testCharge = Int32.Parse(csvData[i][10]);
+                    totalTestCharge += testCharge;
+                }
+                catch { }
+            }
+
+            csvData = csvData.Append(new string[] {
+            "Total", "", "", "", "", "", "", "", totalNoOfMaterial.ToString(), "", totalTestCharge.ToString(),"", "", "", "", ""
+        }).ToArray();
+            AddTable(section, primaryColor, colSizes, colHeads, csvData, fontSize: 8, summationRow: true);
 
             renderer.PrepareRenderPages();
 
